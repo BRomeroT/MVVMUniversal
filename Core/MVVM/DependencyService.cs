7b864495b0da058a8bf3d18c54d65c5ca4588e85
Benjamin Romero <BRomeroT@hotmail.com>
2020-06-15 18:21:38 -0500

    Dependency Service documentation

--- Core/MVVM/DependencyService.cs
+++ Core/MVVM/DependencyService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Sysne.Core.OS
 {
+    /// <summary>
+    /// Allow register dependencies and their lifetime
+    /// </summary>
     public static class DependencyService
     {
         /// <summary>
