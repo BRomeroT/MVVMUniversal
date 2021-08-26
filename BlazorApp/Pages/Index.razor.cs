d2c5127a1e4ecdc4822c80c550f29c1a68f2bff0
Benjamin Romero <BRomeroT@hotmail.com>
2021-08-26 15:13:16 -0500

    Split index (razor/cs)

---
+++ BlazorApp/Pages/Index.razor.cs
@@ -0,0 +1,31 @@
+﻿using Core.ViewModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            (Sysne.Core.OS.DependencyService.Get<Core.Lib.OS.INavigationService>() as BlazorApp.OS.NavigationService)
+                .NavigationManager = NavigationManager;
+        }
+
+        readonly LoginViewModel viewModel = new LoginViewModel();
+        EditContext editContext;
+        private async Task Submit()
+        {
+            await viewModel.LoginCommand.ExecuteAsync();
+            StateHasChanged();
+        }
+    }
+}
