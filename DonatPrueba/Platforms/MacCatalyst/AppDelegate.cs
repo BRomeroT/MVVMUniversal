ec9ca50f8d7c9d39349ab1a4b5628217cad94a88
Angeles Cruz Robles <angelescrobles1019@outlook.com.mx>
2023-09-14 11:28:07 -0600

    Prueba

---
+++ DonatPrueba/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DonatPrueba
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
